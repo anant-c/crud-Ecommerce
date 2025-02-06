31e0147d13e4786c62cdcfa5c709f9ade0ac2e3b
anant-c <canant448@gmail.com>
2025-02-07 00:18:06 +0530

    setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDb } from './config/db.js'
+import path from "path";
+
 
 import productRoutes from "./routes/product.route.js"
 
@@ -12,6 +14,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.json()) //allows us to accept JSON data in the req.body
 
 app.use("/api/products",productRoutes)
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist"))); // serve the static react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 app.listen(PORT, ()=>{
     connectDb();
